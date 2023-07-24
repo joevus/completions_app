7406c8b94563835fce9b17cacd7d3837ba1e6950
Joe Hoskisson <joehoskisson@gmail.com>
2023-07-24 15:54:18 -0400

    add completions controller and routes

---
+++ app/controllers/completions_controller.rb
@@ -0,0 +1,43 @@
+class CompletionsController < ApplicationController
+    class CompletionsController < ApplicationController
+        before_action :set_completion, only: [:update, :destroy]
+      
+        def index
+          @completions = Completion.all
+          render json: @completions
+        end
+      
+        def create
+          @completion = Completion.new(completion_params)
+      
+          if @completion.save
+            render json: @completion, status: :created
+          else
+            render json: @completion.errors, status: :unprocessable_entity
+          end
+        end
+      
+        def update
+          if @completion.update(completion_params)
+            render json: @completion
+          else
+            render json: @completion.errors, status: :unprocessable_entity
+          end
+        end
+      
+        def destroy
+          @completion.destroy
+          head :no_content
+        end
+      
+        private
+      
+        def set_completion
+          @completion = Completion.find(params[:id])
+        end
+      
+        def completion_params
+          params.require(:completion).permit(:student_name, :student_id)
+        end
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :completions, only: [:index, :create, :update, :destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
