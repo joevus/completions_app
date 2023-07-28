6f24b4cdbf3ab49fd80256b300a13717958c51c6
Joe Hoskisson <joehoskisson@gmail.com>
2023-07-28 15:14:05 -0400

    added roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  belongs_to :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+private
+
+  def set_default_role
+    self.role ||= Role.find_by_name('registered')
+  end
 end
