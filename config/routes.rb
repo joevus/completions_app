514d0c1114e0b5e6bd07c067acf7178613cd96e3
Joe Hoskisson <joehoskisson@gmail.com>
2023-07-31 14:55:43 -0400

    update routes per devise docs for customizable controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
 
   resources :completions, only: [:index, :create, :update, :destroy]
 
