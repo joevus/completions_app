48ed118db88d2c1d2e7918a936a09c4aef7341ec
Joe Hoskisson <joehoskisson@gmail.com>
2023-07-31 15:35:18 -0400

    restrict admin user actions to admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :enforce_admin
 
   def index
     @users = User.all
@@ -7,5 +8,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+private
+
+  def enforce_admin
+    unless current_user.role.name == "admin"
+      redirect_to root_path, notice: 'Access Denied, Admins Only'
+    end
   end
 end
