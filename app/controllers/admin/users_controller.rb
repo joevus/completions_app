569621176a7f2da1422faf7a24f5d5f46e333be6
Joe Hoskisson <joehoskisson@gmail.com>
2023-08-01 16:24:29 -0400

    give admin ability to update and delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :enforce_admin, only: [:index, :show]
+  before_action :enforce_admin, only: [:index, :show, :update, :destroy]
 
   def index
     @users = User.all
@@ -10,11 +10,29 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(update_params)
+
+    render :show
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to admin_users_path
+  end
+
 private
 
   def enforce_admin
     unless current_user.role.name == "admin"
       redirect_to root_path, notice: 'Access Denied, Admins Only'
     end
+  end
+
+  def update_params
+    params.require(:user).permit(:role_id)
   end
 end
